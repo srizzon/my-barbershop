a36c8fb9aca82b5b381abe7d1fb3872945bd1456
Samuel Rizzon <shrizzon@gmail.com>
2025-01-25 11:12:18 -0300

    ✨ feat: add ng-zorro-antd and configure initial setup

--- apps/my-barbershop/src/app/app.config.ts
+++ apps/my-barbershop/src/app/app.config.ts
@@ -1,10 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideNzI18n, pt_BR } from 'ng-zorro-antd/i18n';
+
+import { registerLocaleData } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
+import pt from '@angular/common/locales/pt';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+
 import { appRoutes } from './app.routes';
+
+registerLocaleData(pt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideNzI18n(pt_BR),
+    importProvidersFrom(FormsModule),
+    provideAnimationsAsync(),
+    provideHttpClient(),
   ],
 };
